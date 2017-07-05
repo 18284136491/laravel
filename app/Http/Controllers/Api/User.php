d5b6a94f866da42b9957971fb20c679672c6907c
lucky <499820942@qq.com>
2017-07-05 17:16:56 +0800

    laravel

---
+++ app/Http/Controllers/Api/User.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class User extends Controller
+{
+    
+    public function dear()
+    {
+    	echo 'baby of dear';
+    }
+}
